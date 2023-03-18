4fd4df31411e01338a8f5b530c4e51154306f6ae
saracrz <sara.crz10@gmail.com>
2023-03-18 21:28:07 +0000

    Add search to TV shows container

--- src/components/TVShowsSection.jsx
+++ src/components/TVShowsSection.jsx
@@ -1,31 +1,68 @@
-import {useState} from 'react'
+import { useState, useEffect } from "react";
 import { TVShow } from "./TVShow";
-import { FavouritesSection } from './FavouritesSection'
-import { AddFavourite } from './AddFavourite'
+import { useData } from "../hooks/useData.jsx";
+import { FavouritesSection } from "./FavouritesSection";
+import { Search } from "./Search";
+import "./styles.css";
 
-import './styles.css';
+export const TVShowsSection = ({ shows }) => {
+  const {searchShows, results, query, setQuery } = useData();
+  const [favourites, setFavourites] = useState([]);
 
-export const TVShowsSection = ({shows, addFavourite}) => {
-  const [favourites, setFavourites] = useState([]); 
-  
   const addFavouriteShow = (show) => {
     const favouriteList = [...favourites, show];
-    setFavourites(favouriteList)
-  }
+    setFavourites(favouriteList);
+  };
+
+  const removeFavouriteShow = () => {
+    const items = favourites;
+    const removed = items.splice(-1); 
+    const itemsCopy = [...items]
+    setFavourites(itemsCopy);
+  };
+
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    searchShows();
+    setQuery('');
+  };
+
 
   return (
-    <div className='container'>
-      <div className='shows-list'>
-        {shows.map(singleshow => (
-          <div className='single-show'>
-            <TVShow key={singleshow?.id} 
-            image={singleshow?.image?.medium}  
-            />
-            <AddFavourite addFavourite={() => addFavouriteShow(singleshow?.name)}/>
-          </div>
-        ))}
+    <>
+      <div className="container">
+        <div className="shows-list">
+          <>
+            {results.length
+              ? results.map((result) => (
+                  <div className="single-show">
+                    <TVShow
+                      key={result.id}
+                      image={result.image.medium}
+                      addFavourite={() => addFavouriteShow(result?.name)}
+                    />
+                  </div>
+                ))
+              : shows.map((singleshow) => (
+                  <div className="single-show">
+                    <TVShow
+                      key={singleshow?.id}
+                      image={singleshow?.image?.medium}
+                      addFavourite={() => addFavouriteShow(singleshow?.name)}
+                    />
+                  </div>
+                ))}
+          </>
+        </div>
+        <div className="section-fav">
+        <Search value={query} onChange={handleChange} onSubmit={handleSubmit} />
+        <FavouritesSection favourites={favourites} onRemoveFav={removeFavouriteShow}/>
+        </div>
       </div>
-      <FavouritesSection favourites={favourites}/>
-    </div>
-  )
-}
+    </>
+  );
+};
