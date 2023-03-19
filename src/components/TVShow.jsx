6ee318aa903e3375d5530ab4f2732f0c758cc448
saracrz <sara.crz10@gmail.com>
2023-03-19 17:16:40 +0000

    Add hover with tv show info

--- src/components/TVShow.jsx
+++ src/components/TVShow.jsx
@@ -1,11 +1,21 @@
-import {IconHeart} from '@tabler/icons-react'
-import './styles.css'
+import { IconHeart } from "@tabler/icons-react";
+import "./styles.css";
 
-export const TVShow = ({ title, image, addFavourite}) => {
-    return (
-        <div className='tv-show'>
-          <IconHeart onClick={addFavourite}/>
-          <img src={image} />
+export const TVShow = ({ title, genres, image, addFavourite, ratingAverage, ended }) => {
+  const mappedGenres = genres.length && genres.map(genre => `${genre}, `);
+  return (
+    <div className="tv-show">
+      <div className="hover-div">
+        <IconHeart onClick={addFavourite} />
+        <img src={image} />
+        <div className="hover-text">
+          <li>{title}</li>
+          <li>Rating average: {ratingAverage}</li>
+          <li>Genres: {mappedGenres}</li>
+          <li>Date: {ended}</li>
+          More
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
