fe3e7b7a68f3d642f9ea56d91b76001dc3832ed0
saracrz <sara.crz10@gmail.com>
2023-03-20 21:12:43 +0000

    Create getEpisodes and getSeasons method to fetch data

--- src/hooks/useData.jsx
+++ src/hooks/useData.jsx
@@ -3,8 +3,11 @@ import { useState } from "react";
 export const useData = () => {
   const [data, setData] = useState([]);
   const [page, setPage] = useState(0);
-  const [query, setQuery] = useState('')
+  const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
+  const [seasons, setSeasons] = useState([]);
+  const [episodes, setEpisodes] = useState([]);
+  const [loading, setIsLoading] = useState(true);
 
   const increasePage = () => setPage(page + 1);
   const decreasePage = () => setPage(page - 1);
@@ -13,6 +16,7 @@ export const useData = () => {
   const getData = async () => {
     const response = await fetch(`https://api.tvmaze.com/shows?page=${page}`);
     const tvShows = await response.json();
+
     setData(tvShows);
   };
 
@@ -24,7 +28,20 @@ export const useData = () => {
     })
 
    setResults(resultName);
-   return result;
+  }
+
+  const getSeasons = async (id) => {
+    const response = await fetch(`https://api.tvmaze.com/shows/${id}/seasons`);
+    const seasons = await response.json();
+
+    setSeasons(seasons);
+  }
+
+  const getEpisodes = async (id) => {
+    const response = await fetch(`https://api.tvmaze.com/shows/${id}/episodes`);
+    const episodes = await response.json();
+
+    setEpisodes(episodes);
   }
 
 
@@ -32,12 +49,18 @@ export const useData = () => {
     decreasePage,
     increasePage,
     getData,
+    getEpisodes,
+    getSeasons,
     searchShows, 
+    setIsLoading,
     setResults,
     setQuery, 
     data,
+    episodes,
+    loading,
     page,
     query,
     results,
+    seasons,
   };
 };
