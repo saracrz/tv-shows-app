610461b3b190027083ce40f5f97160f0b149ec18
saracrz <sara.crz10@gmail.com>
2023-03-21 19:41:39 +0000

    Update TVShow component

--- src/components/TVShow.jsx
+++ src/components/TVShow.jsx
@@ -1,19 +1,29 @@
 import { IconHeart } from "@tabler/icons-react";
+import { Link } from "react-router-dom";
 import "./styles.css";
 
-export const TVShow = ({ title, genres, image, addFavourite, ratingAverage, ended, url, onClick}) => {
-  const mappedGenres = genres.length && genres.map(genre => `${genre}, `);
+export const TVShow = ({
+  title,
+  genres,
+  image,
+  addFavourite,
+  ratingAverage,
+  ended,
+  url,
+}) => {
+  const mappedGenres = genres.length && genres.map((genre) => `${genre}, `);
 
   return (
     <div className="tv-show">
       <div className="hover-div">
-        {image && <IconHeart onClick={addFavourite} />}
-        {image && <img src={image} />}
+        <IconHeart onClick={addFavourite} />
+        <img src={image} />
         <div className="hover-text">
           <li>{title}</li>
           <li>Rating average: {ratingAverage}</li>
           <li>Genres: {mappedGenres}</li>
           {ended && <li>Date: {ended}</li>}
+          <Link to={url}>Go to seasons</Link>
         </div>
       </div>
     </div>
